1e96c7702734eb4402a5ef9afd5b8acd5fb76284
Ben Simpson <b.albertsimpson@gmail.com>
2020-02-24 15:46:01 -0800

    made minor adjustments to app.py file

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from dictogram import Dictogram
 from listogram import Listogram
+from random import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def generate_words():
-    with open('./EAP.text') as f:
-        words = f.readlines()
-        lines = Dictogram(words)
+    #with open('./EAP.text') as f:
+        #words = f.readlines()
+    lines = Dictogram(['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish'])
     sentence = ""
-    return lines.sample()
-    num_words = 10
+    num_words = 20
+    for i in range(num_words):
+        word = lines.sample()
+        sentence += " " + word
+    return sentence
 
 if __name__ == '__main__':
     app.run()
